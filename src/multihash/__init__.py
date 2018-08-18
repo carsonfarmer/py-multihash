228c5767ab3570f6010da5509c54805469a492c9
Carson Farmer <carson.farmer@gmail.com>
2018-08-17 17:09:27 -0700

    initial working code

--- src/multihash/__init__.py
+++ src/multihash/__init__.py
@@ -1,1 +1,173 @@
-__version__ = '0.1.0'
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+
+"""
+Multihash implementation in Python.
+"""
+
+from typing import Dict, Any, Union, Optional
+import base58
+from . import constants
+from . import varint
+
+
+def to_hex_string(multihash: bytes) -> str:
+    """
+    Convert the given multihash to a hex encoded string.
+    :param multihash: The input multihash as an array of bytes.
+    :return: Output hex string representation of input hash.
+    """
+    return multihash.hex()
+
+
+def from_hex_string(multihash: str) -> bytes:
+    """
+    Convert the given hex encoded string to a multihash.
+    :param multihash: The input multihash as a hex encoded string.
+    :return: Output byte array representation of input hash.
+    """
+    return bytes.fromhex(multihash)
+
+
+def to_b58_string(multihash: bytes) -> str:
+    """
+    Convert the given multihash to a base58 encoded string.
+    :param multihash: The input multihash as an array of bytes.
+    :return: Output Base58 encoded string representation of input hash.
+    """
+    if not isinstance(multihash, bytes):
+        raise TypeError('input must be bytes array')
+    return base58.b58encode(multihash).decode()
+
+
+def from_b58_string(multihash: str) -> bytes:
+    """
+    Convert the given base58 encoded string to a multi-hash.
+    :param multihash: The input multihash as a Bse58 encoded string.
+    :return: Output byte array representation of input hash.
+    """
+    if not isinstance(multihash, str):
+        raise TypeError('input must be string')
+    return base58.b58decode(multihash)
+
+
+# TODO: Should this return a namedtuple instead of a dict to match JS syntax?
+def decode(buf: bytes) -> Dict[str, Any]:
+    """
+    Decode a hash from the given multihash.
+    :param buf: Input multihash as an array of bytes.
+    :return: Dictionary of type {code: int, name: str, length: int, digest: bytes}
+    """
+    if not isinstance(buf, bytes):
+        raise TypeError('multihash should be `bytes` object')
+
+    if len(buf) < 3:
+        raise ValueError('multihash too short. must be > 3 bytes.')
+
+    code, n = varint.from_varint(buf)
+    if not is_valid_code(code):
+        raise ValueError('multihash unknown function code: {code}'.format(code=code))
+
+    length, n = varint.from_varint(buf, n)
+    if length < 1:
+        raise ValueError('multihash invalid length: {length}'.format(length=length))
+
+    buf = buf[n:]
+
+    if len(buf) != length:
+        raise ValueError('multihash length inconsistent: {} != {}'.format(len(buf), length))
+
+    return dict(code=code, name=constants.codes[code], length=length, digest=buf)
+
+
+def encode(digest: bytes, code: Union[str, int], length: Optional[int] = None) -> bytes:
+    """
+    Encode a hash digest along with the specified function code.
+    :param digest: Input hash digest as an array of bytes.
+    :param code: The hash function code as an int or string.
+    :param length: The digest length. Defaults to None, in which case length is derived from the digest itself.
+    :return: Output digest bytes array.
+    """
+    # Ensure it's a hash function code.
+    hash_function = coerce_code(code)
+
+    if not isinstance(digest, bytes):
+        raise TypeError('digest should be `bytes` object')
+
+    length = len(digest) if length is None else length
+
+    if length != len(digest):
+        raise ValueError('digest length should be equal to specified length')
+
+    return varint.to_varint(hash_function) + varint.to_varint(length) + digest
+
+
+def coerce_code(name: Union[str, int]):
+    """
+    Converts a hash function name into the matching code.
+    If passed a number it will return the number if it's a valid code.
+    :param name: The input hash function code.
+    :return: Output hash function number/code.
+    """
+    code = name
+    if isinstance(name, str):
+        if name not in constants.names:
+            raise ValueError('Unrecognized hash function name: {name}'.format(name=name))
+        code = constants.names[name]
+
+    if not isinstance(code, int):
+        raise TypeError('Hash function code should be a number. Got: {code}'.format(code=code))
+
+    if code not in constants.codes and not is_app_code(code):
+        raise ValueError('Unrecognized function code: {code}'.format(code=code))
+
+    return code
+
+
+def is_app_code(code: int) -> bool:
+    """
+    Checks whether a code is part of the app range.
+    :param code: Code to check.
+    :return: Boolean indicating whether `code` is within the app range.
+    """
+    return 0 < code < 0x10
+
+
+def is_valid_code(code: int) -> bool:
+    """
+    Checks whether a multihash code is valid.
+    :param code: Code to check.
+    :return: Boolean indicating whether `code` is a valid multihash code.
+    """
+
+    if is_app_code(code):
+        return True
+
+    if code in constants.codes:
+        return True
+
+    return False
+
+
+def validate(multihash: bytes) -> bool:
+    """
+    Check if the given buffer is a valid multihash.
+    :param multihash: Input multihash bytes array.
+    :return: Boolean indicating if input in  a valid multihash.
+    """
+    try:
+        decode(multihash)
+        return True
+    except ValueError:
+        return False
+
+
+def prefix(multihash: bytes) -> bytes:
+    """
+    Returns a prefix from a valid multihash. Throws an error if it is not valid.
+    :param multihash: Input multihash bytes array.
+    :return: Bytes array with multihash prefix.
+    """
+    if validate(multihash):
+        return multihash[:2]
+    raise ValueError('invalid multihash')
--- tests/test_multihash.py
+++ tests/test_multihash.py
@@ -1,6 +1,182 @@
+"""
+ Multihash tests.
+"""
 
-from multihash.cli import main
+import base58
+import pytest
+import multihash
 
 
-def test_main():
-    assert main([]) == 0
+class TestHexString:
+    def test_valid_to_hex_string(self, valid):
+        for case in valid:
+            code = case['encoding']['code']
+            buf = multihash.encode(bytes.fromhex(case['hex']), code)
+            assert multihash.to_hex_string(buf) == buf.hex()
+
+    def test_invalid_to_hex_string(self):
+        # In actual fact, type checking should catch this for us
+        with pytest.raises(AttributeError):
+            multihash.to_hex_string('hello world')
+
+    def test_valid_from_hex_string(self, valid):
+        for case in valid:
+            code = case['encoding']['code']
+            buf = multihash.encode(bytes.fromhex(case['hex']), code)
+            assert multihash.from_hex_string(buf.hex()) == buf
+
+
+class TestBase58String:
+    def test_valid_to_b58_string(self, valid):
+        for case in valid:
+            code = case['encoding']['code']
+            buf = multihash.encode(bytes.fromhex(case['hex']), code)
+            # Currently a lame test, but good for regression testing if we change our underlying base58 lib
+            assert multihash.to_b58_string(buf) == base58.b58encode(buf).decode()
+
+    def test_invalid_to_b58_string(self):
+        # In actual fact, type checking should catch this for us
+        with pytest.raises(TypeError):
+            multihash.to_b58_string('hello world')
+
+    def test_valid_from_b58_string(self):
+        src = 'QmPfjpVaf593UQJ9a5ECvdh2x17XuJYG5Yanv5UFnH3jPE'
+        expected = bytes.fromhex('122013bf801597d74a660453412635edd8c34271e5998f801fac5d700c6ce8d8e461')
+        assert multihash.from_b58_string(src) == expected
+
+
+def sample(code, size, hexi):
+    return code.to_bytes((code.bit_length() + 7) // 8, byteorder='big') + \
+           size.to_bytes((size.bit_length() + 7) // 8, byteorder='big') + \
+           bytes.fromhex(hexi)
+
+
+class TestDecode:
+    def test_valid_decode(self, valid):
+        for case in valid:
+            code = case['encoding']['code']
+            buf = sample(code, case['size'], case['hex'])
+            name = case['encoding']['name']
+            d1 = bytes.fromhex(case['hex'])
+
+            r = multihash.decode(buf)
+            d2 = r['digest']
+
+            assert r['code'] == code
+            assert r['name'] == name
+            assert r['length'] == len(d1)
+            assert d1 == d2
+
+    def test_invalid_decode(self):
+        # In actual fact, type checking should catch this for us
+        with pytest.raises(TypeError):
+            multihash.decode('hello')
+
+class TestEncode:
+    def test_valid_encode(self, valid):
+        for case in valid:
+            code = case['encoding']['code']
+            buf = sample(code, case['size'], case['hex'])
+            name = case['encoding']['name']
+            results = [
+                multihash.encode(bytes.fromhex(case['hex']), code),
+                multihash.encode(bytes.fromhex(case['hex']), name)
+            ]
+
+            for result in results:
+                assert result.hex() == buf.hex()
+
+    def test_invalid_encode(self):
+        # In actual fact, type checking should catch these for us
+        with pytest.raises(TypeError):
+            multihash.encode()
+        with pytest.raises(TypeError):
+            multihash.encode('hello', 0x11)
+        with pytest.raises(ValueError):
+            multihash.encode('hello'.encode(), 0x11, 2)
+
+
+class TestValidate:
+    def test_valid_validate(self, valid):
+        for case in valid:
+            assert multihash.validate(sample(case['encoding']['code'], case['size'], case['hex'])) is True
+
+    def test_invalid_validate(self, invalid):
+        for case in invalid:
+            assert multihash.validate(sample(case['code'], case['size'], case['hex'])) is False
+
+        # bytes array of length 150
+        long_buffer = bytes(150)
+        assert multihash.validate(long_buffer) is False
+
+
+class TestIsValidCode:
+    def test_valid_is_valid_code(self):
+        assert multihash.is_valid_code(2) is True
+        assert multihash.is_valid_code(0x13) is True
+
+    def test_invalid_is_valid_code(self):
+        assert multihash.is_valid_code(0x10) is False
+        assert multihash.is_valid_code(0x90) is False
+
+
+class TestIsAppCode:
+    def test_valid_is_app_code(self):
+        for n in range(1, 0x10):
+            assert multihash.is_app_code(n) is True
+
+    def test_invalid_is_app_code(self):
+        assert multihash.is_app_code(0) is False
+
+        for m in range(0x10, 0xff + 1):
+            assert multihash.is_app_code(m) is False
+
+
+class TestCoerceCode:
+    def test_valid_coerce_code(self):
+        names = {
+            'sha1': 0x11,
+            'sha2-256': 0x12,
+            'sha2-512': 0x13,
+            'sha3-512': 0x14
+        }
+        for name in names:
+            assert multihash.coerce_code(name) == names[name]
+            # Should also work for codes
+            assert multihash.coerce_code(names[name]) == names[name]
+
+    def test_invalid_coerce_code(self):
+        invalid_names = [
+            'sha256',
+            'sha9',
+            'Blake4b'
+        ]
+
+        for name in invalid_names:
+            with pytest.raises(ValueError):
+                multihash.coerce_code(name)
+
+        with pytest.raises(TypeError):
+            multihash.coerce_code(b'hello world')
+
+        with pytest.raises(ValueError):
+            multihash.coerce_code(0x99)
+
+
+class TestPrefix:
+    def test_valid_prefix(self):
+        mhash = multihash.encode(b'hey', 0x11, 3)
+        prefix = multihash.prefix(mhash)
+        assert prefix.hex() == '1103'
+
+    def test_invalid_prefix(self):
+        mhash = b'definitely not valid'
+        with pytest.raises(ValueError):
+            multihash.prefix(mhash)
+
+class TestConstants:
+    def test_has_constants(self):
+        from multihash.constants import names, codes, default_lengths
+        assert len(names.keys()) > 0
+        assert len(codes.keys()) > 0
+        assert len(default_lengths.keys()) > 0
